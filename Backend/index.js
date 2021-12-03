7e4ad8d1a887bca9665dbb95b3f9b9dcd19fe39e
Eemil Vaananen <eemil.vaananen@tuni.fi>
2021-12-03 14:07:46 +0200

    test

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+app.use(cors());
+const mysql = require("mysql");
+
+let config = {
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
+const port = process.env.PORT || 8080;
+
+var pool = mysql.createPool(config);
+app.get("/", (req, res) => {
+  pool.query("SELECT * from location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
+});
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${server.address().port}`);
+});
