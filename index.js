90aefdfbe4dd5b7755bc605dec606ae43a43d373
Eemil Vaananen <eemil.vaananen@tuni.fi>
2021-12-03 14:12:33 +0200

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 8080;
 
 var pool = mysql.createPool(config);
 app.get("/", (req, res) => {
-  pool.query("SELECT * from location", (error, results) => {
+  pool.query("SELECT * from locations", (error, results) => {
     if (error) {
       console.log(error);
     } else {
